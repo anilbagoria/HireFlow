bde697a3be4debe968409efe41aaf0b00dcadead
anilbagoria <anilbagoria79@gmail.com>
2026-07-12 18:10:33 +0300

    Update CORS for Vercel

--- backend/index.js
+++ backend/index.js
@@ -9,23 +9,26 @@ import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 import interviewRoute from "./routes/interview.route.js";
 
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: [
+        "http://localhost:5173",
+        "https://hire-flow-lake-nine.vercel.app"
+    ],
+    credentials: true
+};
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-
 
 // api's
 app.use("/api/v1/user", userRoute);
@@ -34,9 +37,7 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 app.use("/api/v1/interview", interviewRoute);
 
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
